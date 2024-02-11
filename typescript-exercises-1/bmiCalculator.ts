e4a4f991bd87edecce16fec0f3072dd1e3e17a17
Parsakaali <a@a.com>
2024-02-11 12:55:09 +0200

    9.3 done'

---
+++ typescript-exercises-1/bmiCalculator.ts
@@ -0,0 +1,63 @@
+interface BmiValues {
+    height: number;
+    weight: number;
+  }
+
+const calculateBmi = (height: number, weight: number) : string => {
+    let bmi = weight / ((height / 100)**2)
+
+    let returnString = `Your body mass index: ${bmi}\n`
+
+    if(bmi > 40)
+        returnString += "Obese (Class III)"
+
+    else if(bmi >= 35)
+        returnString +=  "Obese (Class II)"
+
+    else if(bmi >= 30)
+        returnString +=  "Obese (Class I)"
+
+    else if(bmi >= 25)
+        returnString +=  "Overweight (Pre-obese)"
+
+    else if(bmi >= 18.5)
+        returnString +=  "Normal range"
+
+    else if(bmi >= 17.0)
+        returnString +=  "Underweight (Mild thinness)"
+
+    else if(bmi >= 16.0)
+        returnString +=  "Underweight (Moderate thinness)"
+
+    else if(bmi < 16.0)
+        returnString +=  "Underweight (Severe thinness)"
+
+    return returnString
+}
+
+const parseArguments = (args: string[]): BmiValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+  
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+      return {
+        height: Number(args[2]),
+        weight: Number(args[3])
+      }
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+  }
+
+try {
+    const { height, weight } = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+  } 
+  
+  catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+  }
---
+++ typescript-exercises-1/exerciseCalculator.ts
@@ -0,0 +1,67 @@
+interface results {
+    days: number,
+    trainingDays: number,
+    targetHours: number,
+    averageHours: number,
+    targetReached: boolean,
+    rating: number,
+    feedback: string
+}
+
+const calculateExercise = (trainingData: number[], target: number):results => 
+{
+    const days = trainingData.length
+    const trainingDays = trainingData.filter(hours => hours > 0).length
+
+    const sum = trainingData.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    const averageHours = sum / trainingData.length;
+
+    const targetReached = averageHours >= target
+    let rating;
+    let ratingFeedback;
+
+    if(averageHours >= target)
+    {
+        rating = 3;
+        ratingFeedback = "Well done, you've reached your target!"
+    }
+
+    else if(averageHours >= target * 0.8)
+    {
+        rating = 2;
+        ratingFeedback = "You almost reached your target!"
+    }
+
+    else
+    {
+        rating = 1;
+        ratingFeedback = "Lazy!"
+    }
+
+    return{
+        days: days,
+        trainingDays: trainingDays,
+        targetHours: target,
+        averageHours: averageHours,
+        targetReached: targetReached,
+        rating: rating,
+        feedback: ratingFeedback
+    }
+}
+
+const cliArgs = process.argv.slice(3);
+const numbers = cliArgs.map(arg => parseFloat(arg));
+
+const target = Number(process.argv[2])
+
+if (isNaN(target))
+    console.log("Invalid target")
+
+else{
+    if (numbers.some(isNaN)) {
+    console.log("Please provide valid numbers as command-line arguments.");
+    } else {
+    console.log(calculateExercise(numbers, target))
+    }
+}
+
