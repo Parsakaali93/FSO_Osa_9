9dbba611ac42560085de65691bdad5606f6f8574
Parsakaali <a@a.com>
2024-02-13 14:40:27 +0200

    9.9 done

---
+++ patientor-backend/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+import cors from 'cors';
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here');
+  return res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
